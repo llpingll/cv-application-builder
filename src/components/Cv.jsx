975399de7876c9d988981e8e34878e092e849a16
llpingll <94099281+llpingll@users.noreply.github.com>
2024-01-08 11:46:53 +0000

    Add cv preview styling

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -6,9 +6,20 @@ function Cv() {
     <div className="cv">
       <div className="cv-personal-info">
         <p className="cv-fullname">Full Name:</p>
-        <p>Email:</p>
-        <p>Phone Number:</p>
-        <p>Address:</p>
+        <div className="cv-contact-info">
+          <div>
+            <i className="fa-solid fa-location-dot" />
+            <p>Email:</p>
+          </div>
+          <div>
+            <i className="fa-solid fa-phone" />
+            <p>Phone Number:</p>
+          </div>
+          <div>
+            <i className="fa-solid fa-house" />
+            <p>Address:</p>
+          </div>
+        </div>
       </div>
       <div />
       <div />
